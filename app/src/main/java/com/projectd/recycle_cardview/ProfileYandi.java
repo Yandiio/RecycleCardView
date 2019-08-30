4fc9a60bb88bebcb7670edea739ddaf163780226
yandiio <yandifenanda95@gmail.com>
2019-08-30 21:07:29 +0700

    Yandiio Profile Added

--- app/src/main/java/com/projectd/recycle_cardview/ProfileYandi.java
+++ app/src/main/java/com/projectd/recycle_cardview/ProfileYandi.java
@@ -51,9 +51,6 @@ public class ProfileYandi extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //no inspection SimplifiableIfStatement
